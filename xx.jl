26535bfdad4da875a0b1bd9ab421e64712ee7ff5
Pawel Chojnacki <pawel@chojnacki.ws>
2014-12-13 23:18:14 +0100

    Ha

--- xx.jl
+++ xx.jl
@@ -1,31 +1,13 @@
 2+2
 using Gadfly, MPFI
-A =  Interval[ 1.0 -1 2 -1 -8
+A =  BigFloat[ 1.0 -1 2 -1 -8
        2 -2 3 -3 -20
        1  1 1  0  -2
        1 -1 4  3  4 ]
 
-import Base.isless, Base.promote_rule, Base.convert
-
-promote_rule(::Type{Interval}, ::Type{Int64}) = Interval
-promote_rule(::Type{Interval}, ::Type{Float64}) = Interval
-
-
-# Base.promote_type(Interval, Float64)
-# promote_rule(::Type{Int64}, ::Type{Interval}) = Interval
-typeof([2])
-promote(Interval("1.1"), 1)
-
-Interval("1.1") + Int64[1]
-
-function isless(a::Interval, b::Interval)
-  true
-end
-
-
 function reduce_augmented_matrix(A)
   n = size(A,1)
-  X = zeros(n)
+  X = zeros(eltype(A), n)
   X[n] = A[n,n+1]/A[n,n]
   for i = n-1:-1:1
     X[i] = (A[i,n+1] - sum([A[i,j]*X[j] for j = i+1:n ])) / A[i,i]
@@ -49,9 +31,10 @@ function reduce_row(A, i)
   end
 end
 
-
 function gaussian_backwards_substitution(A)
   A=copy(A)
+  Aint = convert(Array{Interval, 2}, A)
+  println(Aint);
   n = size(A,1)
   for i = 1:n-1
     p = findfirst(A[i:n,i]) # find first nonzero
@@ -64,12 +47,15 @@ function gaussian_backwards_substitution(A)
     subst(A, p, i)
     reduce_row(A,i)
 
+    subst(Aint, p, i)
+    reduce_row(Aint, i)
+
     if A[n,n] == 0
       println("no unique solution exists")
       return 0;
     end
   end
-
+  println("x: ", reduce_augmented_matrix(Aint))
   return reduce_augmented_matrix(A);
 end
 
@@ -79,7 +65,7 @@ function gaussian_partial_pivoting(A)
   for i = 1:n-1
     abs_array = [abs(A[j,i]) for j = i:n]
     p = findfirst(abs_array, maximum(abs_array)) + i-1
-
+    println(abs_array)
     if A[p, i] == 0
       println("no unique solution exists")
       return 0
@@ -93,12 +79,6 @@ function gaussian_partial_pivoting(A)
 end
 
 
-X = gaussian_partial_pivoting(A)
-
-# println(A[:,1:n] \ A[:,n+1])
-println(X)
 with_bigfloat_precision(50) do
-  println(gaussian_partial_pivoting(A))
+  println(gaussian_backwards_substitution(A))
 end
-
-
